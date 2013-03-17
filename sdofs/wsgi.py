c958ba516eb43a0d0c36271a342136293ff3406f
Praveen_AWS <praveen97uma@gmail.com>
2013-03-17 01:47:06 +0000

    Correct wsgi application.

--- sdofs/wsgi.py
+++ sdofs/wsgi.py
@@ -13,15 +13,16 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
+sys.path.append('/home/ubuntu/sdofs')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdofs.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.handlers import wsgi
+application = wsgi.WSGIHandler()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
