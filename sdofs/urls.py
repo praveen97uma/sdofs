c37ab85c3a30823ea5d037487feb52397c3bb149
Praveen_AWS <praveen97uma@gmail.com>
2013-03-17 01:46:26 +0000

    Map view to url.

--- sdofs/urls.py
+++ sdofs/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+import views
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sdofs.views.home', name='home'),
+     url(r'^$', views.home, name='home'),
     # url(r'^sdofs/', include('sdofs.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
 )
