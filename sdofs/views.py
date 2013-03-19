090f9d43be172ac278ee2487e5885e16f305932b
Praveen_AWS <praveen97uma@gmail.com>
2013-03-19 09:10:44 +0000

    test function

--- sdofs/views.py
+++ sdofs/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from fandjango.decorators import facebook_authorization_required as fb_authorize
 
+@fb_authorize
 def home(request):
-    
+	return HttpResponse("Hey! How you doing")    
