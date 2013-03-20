dc65d098c75d000d3569776b4254cf37cfaf0847
Praveen Kumar <praveen97uma@gmail.com>
2013-03-21 04:00:03 +0530

    Move status update logic to tasks module.

--- sdofs/apps/miner/views.py
+++ sdofs/apps/miner/views.py
@@ -11,6 +11,7 @@ from miner.models import Post
 from miner.models import Comment
 from miner.models import UsersVisited
 
+from miner.tasks import fetchStatuses
 
 def userAlreadySuppliedInfo(user):
     try:
@@ -27,40 +28,7 @@ def miner(request):
     current_user = request.facebook.user
     if userAlreadySuppliedInfo(current_user):
         return playGame(request)
-
-    graph = current_user.graph
-    # extend the expiry of the access token that the user has granted
-    #current_user.oauth_token.extend()
-    user_id = current_user.facebook_id
-    FEED_URL = 'me/statuses'
-    statuses = graph.get(FEED_URL, limit=100) 
-    data = statuses.get('data', [])
-    for status in data:
-        like_count = 0
-        if status.has_key('likes'):
-            like_count = len(status['likes']['data'])
-        status_data = {
-            'post_id': status['id'],
-            'message': status['message'],
-            'like_count': like_count,
-            'user_id': user_id,
-        }
-        post = Post(**status_data)
-        post.save()
-        if status.has_key('comments'):
-            all_comments = status.get('comments', []).get('data', [])
-            for comment in all_comments:
-                comment_data = {
-                    'comment_id': comment.get('id', '0'),
-                    'user_id': (comment.get('from')).get('id', '0'),
-                    'message': comment.get('message', None),
-                    'user_likes': int(comment.get('user_likes') == 'true'),
-                    'like_count': comment.get('like_count', 0),
-                    'post': post,
-                }
-                comment_entity = Comment(**comment_data)
-                comment_entity.save()
-    
+    fetchStatuses.delay(current_user)
     visited_user = UsersVisited(facebook_id=user_id, user=current_user)
     visited_user.save()
     return playGame(request)
