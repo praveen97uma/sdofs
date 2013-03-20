4cb85634b111b804da89e031e7196791073ddc7f
Praveen Kumar <praveen97uma@gmail.com>
2013-03-20 23:07:32 +0530

    Add code for saving user data in the database.

--- sdofs/apps/miner/views.py
+++ sdofs/apps/miner/views.py
@@ -1,28 +1,45 @@
 from django.http import HttpResponse
 from fandjango.decorators import facebook_authorization_required as fb_authorize
 import json
-FB_GRAPH_API_URL = 'https://graph.facebook.com/'
+
+from miner.models import Post, Comment
 
 @fb_authorize
 def miner(request):
-    #if not request.facebook.user:
-    #    return HttpResponse('Not authorized')
     current_user = request.facebook.user
     graph = current_user.graph
     # extend the expiry of the access token that the user has granted
     #current_user.oauth_token.extend()
     user_id = current_user.facebook_id
     FEED_URL = 'me/statuses'
-    status = graph.get(FEED_URL)
-    statuses =status.keys()
-    #for s in status:
-    #	statuses.append(s)
-    #statuses.append(status.next())
+    statuses = graph.get(FEED_URL, limit=100) 
+    data = statuses.get('data', [])
+    for status in data:
+        like_count = 0
+        if status.has_key('likes'):
+            like_count = len(status['likes']['data'])
+        status_data = {
+            'post_id': status['id'],
+            'message': status['message'],
+            'like_count': like_count,
+            'user_id': user_id,
+        }
+        post = Post(**status_data)
+        post.save()
+        if status.has_key('comments'):
+            all_comments = status.get('comments', []).get('data', [])
+            for comment in all_comments:
+                comment_data = {
+                    'comment_id': comment.get('id', '0'),
+                    'user_id': (comment.get('from')).get('id', '0'),
+                    'message': comment.get('message', None),
+                    'user_likes': int(comment.get('user_likes') == 'true'),
+                    'like_count': comment.get('like_count', 0),
+                    'post': post,
+                }
+                comment_entity = Comment(**comment_data)
+                comment_entity.save()
     response = ""
-    if request.facebook.user:
-        response = "Hi, %s!"%request.facebook.user.facebook_username
-    else:
-        response = "Go Away"
-    return HttpResponse(response+json.dumps(statuses))
+    return HttpResponse(response)
 
 
