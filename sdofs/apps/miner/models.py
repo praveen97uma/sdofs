f68125bf38543b2c74a9792074afa42899681e8b
Praveen Kumar <praveen97uma@gmail.com>
2013-03-20 23:04:41 +0530

    Add Post and Comment model.

--- sdofs/apps/miner/models.py
+++ sdofs/apps/miner/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Comment(models.Model):
-    """Represents a comment on a post.
-    """
-    comment_id = models.CharField(max_length=255, unique=True, blank=False, null=False)
-    user_id = models.CharField(max_length=255)
-    message = models.TextField(null=True, blank=True)
-    created_time = models.DateTimeField()
-
 
 class Post(models.Model):
     """Represents a post in a user's feed.
@@ -15,7 +7,15 @@ class Post(models.Model):
     post_id = models.CharField(max_length=255, unique=True, blank=False, null=False)
     user_id = models.CharField(max_length=255, blank=True, null=True)
     message = models.TextField(null=True, blank=True)
-    _type = models.CharField(max_length=255, blank=True, null=True)
-    created_time = models.DateTimeField()
-    updated_time = models.DateTimeField()
-    likes = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    """Represents a comment on a post.
+    """
+    comment_id = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    user_id = models.CharField(max_length=255)
+    message = models.TextField(null=True, blank=True)
+    like_count = models.IntegerField(default=0)
+    user_likes = models.IntegerField(default=0)
+    post = models.ForeignKey(Post)
